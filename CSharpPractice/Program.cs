8935895471c3b185a7337001f1e54a9b3599e363
nanumhk <nanumhk@gmail.com>
2021-01-26 18:17:06 -0500

    initial commit

---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num_of_apples = 10;
+            double price_of_an_apples = 0.25;
+            double total_price = num_of_apples * price_of_an_apples;
+
+              
+            Console.WriteLine("Total price is " + total_price);
+        }
+    }
+}
